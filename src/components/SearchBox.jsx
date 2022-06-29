e16b4d13c5386503ec42bee703bea95a3245d190
Vikram Karanth <vikram.karanth@cevo.com.au>
2022-06-29 11:33:08 +1000

    searchbox changes

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,17 +4,16 @@ import FormControl from 'react-bootstrap/FormControl';
 
 const SearchBox = (props) => {
 	return (
-		<>
-            <Form.Control
-            style={{width: '15%'}}
-            value={props.value}
-            onChange={(event) => props.setSearchValue(event.target.value)}
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-            />
-		</>
+		<Form className="d-flex">
+      <FormControl
+      value={props.value}
+      onChange={(event) => props.setSearchValue(event.target.value)}
+      type="search"
+      placeholder="Search"
+      className="me-2"
+      aria-label="Search"
+      />
+		</Form>
 	);
 };
 
